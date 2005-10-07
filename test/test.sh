ef981f6d4a1ac680d9d52b325a121d0840d67e5c
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2005-10-07 08:47:46 +0000

    after migration ...

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-rfcp /castor/cern.ch/cms/archive/ecal/h4/H4-000008203-SM5-PEDESTAL-STD /tmp/dellaric/
-rfcp /castor/cern.ch/cms/archive/ecal/h4/H4-000008205-SM5-TEST_PULSE-MGPA /tmp/dellaric/
-rfcp /castor/cern.ch/cms/archive/ecal/h4/H4-000008206-SM5-COSMIC-STD /tmp/dellaric/
+rfcp /castor/cern.ch/cms/archive/ecal/h4/H4-000008203-SM5-PEDESTAL-STD /tmp/$USER/
+ln -s /tmp/$USER/H4-000008203-SM5-PEDESTAL-STD .
+rfcp /castor/cern.ch/cms/archive/ecal/h4/H4-000008205-SM5-TEST_PULSE-MGPA /tmp/$USER/
+ln -s /tmp/$USER/H4-000008205-SM5-TEST_PULSE-MGPA .
+rfcp /castor/cern.ch/cms/archive/ecal/h4/H4-000008206-SM5-COSMIC-STD /tmp/$USER/
+ln -s /tmp/$USER/H4-000008206-SM5-COSMIC-STD .
 cmsRun --parameter-set config.txt
