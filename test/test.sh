4272d8238a5d8028b280293f62ae3e374878f684
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2005-10-07 09:42:30 +0000

    few more fixes

--- test/test.sh
+++ test/test.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-rfcp /castor/cern.ch/cms/archive/ecal/h4/H4-000008203-SM5-PEDESTAL-STD /tmp/$USER/
-ln -s /tmp/$USER/H4-000008203-SM5-PEDESTAL-STD .
-rfcp /castor/cern.ch/cms/archive/ecal/h4/H4-000008205-SM5-TEST_PULSE-MGPA /tmp/$USER/
-ln -s /tmp/$USER/H4-000008205-SM5-TEST_PULSE-MGPA .
-rfcp /castor/cern.ch/cms/archive/ecal/h4/H4-000008206-SM5-COSMIC-STD /tmp/$USER/
-ln -s /tmp/$USER/H4-000008206-SM5-COSMIC-STD .
-cmsRun --parameter-set config.txt
+for FILE in H4-000008203-SM5-PEDESTAL-STD H4-000008205-SM5-TEST_PULSE-MGPA H4-000008206-SM5-COSMIC-STD ; do
+  echo $FILE
+  rm -f /tmp/$USER/$FILE
+  rfcp /castor/cern.ch/cms/archive/ecal/h4/$FILE /tmp/$USER/
+  ln -sf /tmp/$USER/$FILE /tmp/
+done
+
+cmsRun --parameter-set dump.txt
