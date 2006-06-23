7ddd5bb777502cf6fec3121a6a5706681d2baddb
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2006-06-23 14:43:01 +0000

    - comment out the XdaqCollector bits

--- test/.runMonitorModule.sh
+++ test/.runMonitorModule.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-./sendSimpleCmdToApp .host .portn XdaqCollector         0 Configure
+#./sendSimpleCmdToApp .host .portn XdaqCollector         0 Configure
 
 ./sendSimpleCmdToApp .host .portn FilterUnitFramework   0 Configure
 
 ./sendSimpleCmdToApp .host .portn evf::FUEventProcessor 0 Configure
 
-./sendSimpleCmdToApp .host .portn XdaqCollector         0 Enable
+#./sendSimpleCmdToApp .host .portn XdaqCollector         0 Enable
 
 ./sendSimpleCmdToApp .host .portn FilterUnitFramework   0 Enable
 
